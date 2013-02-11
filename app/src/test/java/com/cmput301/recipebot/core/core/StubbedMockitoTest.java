0d897ef92db0fba251b828cf3cfb51fe9086dab3
Prateek Srivastava <f2prateek@gmail.com>
2013-02-11 09:42:12 -0700

    Add stubbed unit tests

---
+++ app/src/test/java/com/cmput301/recipebot/core/core/StubbedMockitoTest.java
@@ -0,0 +1,48 @@
+
+
+package com.cmput301.recipebot.core.core;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+
+import static org.mockito.Mockito.verify;
+
+/**
+ * Stubbed unit tests
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class StubbedMockitoTest {
+
+
+    @Mock
+    private OutputStream outputStream;
+
+    /**
+     * Set up default mocks
+     *
+     * @throws IOException
+     */
+    @Before
+    public void before() throws IOException {
+
+    }
+
+    /**
+     * Stubbed Test
+     *
+     * @throws IOException
+     */
+    @Test
+    public void getStubbedTest() throws IOException {
+        OutputStreamWriter outputStreamWriter = new OutputStreamWriter(outputStream);
+        outputStreamWriter.close();
+        verify(outputStream).close();
+    }
+}
---
+++ app/src/test/java/com/cmput301/recipebot/core/core/StubbedUnitTest.java
@@ -0,0 +1,16 @@
+package com.cmput301.recipebot.core.core;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Unit tests of client API
+ */
+public class StubbedUnitTest {
+
+    @Test
+    public void stubbedTest() throws Exception {
+        assertEquals(5 * 1, 5);
+    }
+}
